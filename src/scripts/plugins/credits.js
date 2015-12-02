b4fb577042a3da15a28bd6d56cb604b9c2838237
Minecheesecraft <felinecraft@gmail.com>
2015-12-02 08:17:20 -0600

    Fixed spacing.

--- src/scripts/plugins/credits.js
+++ src/scripts/plugins/credits.js
@@ -2,17 +2,17 @@ module.exports.inject = inject;
 ////Do not delete/edit this plugin! This logs chat to the console!
 
 function inject(bot) {
-	////Fake console
-	function fakeConsole(text,type) {
-		document = doc = window.document 
-		var text = arguments[0]
-		var type = arguments[1]
-		var para = document.createElement("P");
-		var t = document.createTextNode(text);
-		para.className = type;
-		para.appendChild(t);
-		document.getElementById('Space').appendChild(para);
-		window.scrollTo(0,document.body.scrollHeight);
-		}
-	fakeConsole("Cheese Bot was created by Minecheesecraft. Cheese Bot also uses code from Andrew Kelley, Casey Kuball, and everyone who made PrismarineJS, Node.js, and Node-Webkit. Logo image made by Voxelizer.","info")
+  ////Fake console
+  function fakeConsole(text,type) {
+    document = doc = window.document 
+    var text = arguments[0]
+    var type = arguments[1]
+    var para = document.createElement("P");
+    var t = document.createTextNode(text);
+    para.className = type;
+    para.appendChild(t);
+    document.getElementById('Space').appendChild(para);
+    window.scrollTo(0,document.body.scrollHeight);
+    }
+  fakeConsole("Cheese Bot was created by Minecheesecraft. Cheese Bot also uses code from Andrew Kelley, Casey Kuball, and everyone who made PrismarineJS, Node.js, and Node-Webkit. Logo image made by Voxelizer.","info")
 }
